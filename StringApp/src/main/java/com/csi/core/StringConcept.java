e205497fd97669cf6f7f9af55488cbee4b4d74ff
Satyam1233 <satyamgopale33@gmail.com>
2022-05-18 17:59:38 +0530

    Data added by satyam

---
+++ StringApp/src/main/java/com/csi/core/StringConcept.java
@@ -0,0 +1,13 @@
+package com.csi.core;
+
+public class StringConcept {
+public static void main(String[] args) {
+	
+
+	String s1= "RAMESH";
+	String s2="RAMESH";
+			
+	
+	System.out.println(s1==s2);
+	System.out.println(s1.equals(s2));
+}}
